37433fa064b9011ef6639153953f0f9c4232f5b8
Azul <svcredblu@gmail.com>
2020-11-14 21:06:15 -0600

    created home php file to start displaying the html

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+require_once 'mustache/mustache/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+$m = new Mustache_Engine;
+
+$header = file_get_contents('templates/header.html');
+// $body = file_get_contents('templates/home.html');
+$footer = file_get_contents('templates/footer.html');
+
+$header_data = ["title" => "Home"];
+// $body_data = ["hellowrld" => "Hello World", "name" => "Home Page"];
+
+// $footer_data = [
+//     "localtime" => date('l jS \of F Y h:i:s A'),
+//     "footertitle" => "Home Page"];
+
+echo $m->render($header, $header_data) . PHP_EOL;
+echo $m->render($body, $body_data) . PHP_EOL;
+echo $m->render($footer) . PHP_EOL;
+
+?>
