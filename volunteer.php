2714b37f2f04a00fa11dd117277845b9f8214860
Azul <svcredblu@gmail.com>
2020-11-24 16:56:04 -0600

    adding the volunteer page

---
+++ volunteer.php
@@ -0,0 +1,17 @@
+<?php
+require_once 'mustache/mustache/src/Mustache/Autoloader.php';
+Mustache_Autoloader::register();
+
+$m = new Mustache_Engine;
+
+$header = file_get_contents('templates/header.html');
+$body = file_get_contents('templates/volunteer.html');
+$footer = file_get_contents('templates/footer.html');
+
+$header_data = ["title" => "Volunteer"];
+
+echo $m->render($header, $header_data) . PHP_EOL;
+echo $m->render($body) . PHP_EOL;
+echo $m->render($footer) . PHP_EOL;
+
+?>
