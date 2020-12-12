00438e621a7ed0cfaa478355cdf942ab9e2d8214
Azul <svcredblu@gmail.com>
2020-12-11 23:10:53 -0600

    modified to only include main.js script when needed

--- index.php
+++ index.php
@@ -10,10 +10,6 @@ $footer = file_get_contents('templates/footer.html');
 
 $header_data = ["title" => "Home"];
 
-// $footer_data = [
-//     "localtime" => date('l jS \of F Y h:i:s A'),
-//     "footertitle" => "Home Page"];
-
 echo $m->render($header, $header_data) . PHP_EOL;
 echo $m->render($body) . PHP_EOL;
 echo $m->render($footer) . PHP_EOL;
